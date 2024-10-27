7455ac58fa35591d71651748f312cb3ec13e4b0b
MasterYousef <140777033+MasterYousef@users.noreply.github.com>
2024-10-27 03:27:42 +0300

    Update page.tsx

--- app/Profile/page.tsx
+++ app/Profile/page.tsx
@@ -3,16 +3,16 @@ import React from "react";
 import GetUserAction from "@/actions/GetUserAction";
 import Title from "../components/utils/Title";
 import "@/style/profile.css";
-// import Edit from "../components/profile/EditProfile";
-import GetBookedTours from "@/logic/profile/GetBookedTours";
-import TourCard from "../components/tours/TourCard";
-import AdminBookedTours from "../components/profile/AdminBookedTours";
+import Edit from "../components/profile/EditProfile";
+// import GetBookedTours from "@/logic/profile/GetBookedTours";
+// import TourCard from "../components/tours/TourCard";
+// import AdminBookedTours from "../components/profile/AdminBookedTours";
 import { redirect } from "next/navigation";
 
 async function page() {
   const { user, token } = await GetUserAction();
   if(token){
-    const res = await GetBookedTours(user._id, token, user.role);
+    // const res = await GetBookedTours(user._id, token, user.role);
     return (
       <div className="main p-10">
         <div className="w-full flex flex-col p-5 profile rounded justify-center items-center mb-10 ">
@@ -27,12 +27,12 @@ async function page() {
             <p className="text-2xl font-bold mb-3">{user?.username}</p>
             <p className="mb-5">{user?.email}</p>
           </div>
-          {/* <Edit user={user} token={token} /> */}
+          <Edit user={user} token={token} />
         </div>
         <div className="w-full flex flex-col items-center">
           <Title text={"Booked tours"} />
           <div className="w-full my-5 flex flex-wrap">
-            {res.result >= 1 ? (
+            {/* {res.result >= 1 ? (
               user.role === "admin" ? res.data.map((e) => <AdminBookedTours order={e} token={token}/>) : (
                 res.data.map((e) => <TourCard tour={e.tour} />)
               )
@@ -40,7 +40,7 @@ async function page() {
               <p className="my-16 text-xl font-serif text-center w-full">
                 you didn't book any tour yet
               </p>
-            )}
+            )} */}
           </div>
         </div>
       </div>
