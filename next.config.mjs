f3abbd1373a62b8b64a1ae9be862290ca59e9e19
MasterYousef <140777033+MasterYousef@users.noreply.github.com>
2024-10-27 03:47:15 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: '**',
+        hostname: 'https://egypt-tours-frontend.vercel.app',
       },
     ],
   },
