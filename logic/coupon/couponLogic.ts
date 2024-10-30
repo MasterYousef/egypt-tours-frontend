d9617f1bc822b699dabac3eea0b63059fd95ebb4
MasterYousef <140777033+MasterYousef@users.noreply.github.com>
2024-10-30 07:44:22 +0300

    Update couponLogic.ts

--- logic/coupon/couponLogic.ts
+++ logic/coupon/couponLogic.ts
@@ -5,7 +5,7 @@ import { coupon, couponRes } from "@/types/types"
 async function couponLogic(token:string,page:number=1) {
     let coupons:couponRes|undefined;
     const getData = async()=>{
-        const res = await useGetData<couponRes>(`/api/v1/coupon?limit=4&page=${page}`,token)
+        const res = await useGetData<couponRes>(`/api/v1/coupon?limit=3&page=${page}`,token)
         if(res.status === "success"){
             coupons = res
         }else{
