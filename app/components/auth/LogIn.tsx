b21fea716c631f18c88e9581b7a4e4c1cc496139
MasterYousef <140777033+MasterYousef@users.noreply.github.com>
2024-10-30 07:37:34 +0300

    add show password eye

--- app/components/auth/LogIn.tsx
+++ app/components/auth/LogIn.tsx
@@ -1,66 +1,87 @@
-'use client';
-import React, { useState ,Fragment } from 'react'
-import Title from '../utils/Title'
-import loginLogic from '@/logic/auth/login';
-import { Dialog, Transition } from '@headlessui/react';
+"use client";
+import React, { useState, Fragment } from "react";
+import Title from "../utils/Title";
+import loginLogic from "@/logic/auth/login";
+import { Dialog, Transition } from "@headlessui/react";
 function LogIn() {
-  const logic = loginLogic()
+  const logic = loginLogic();
+  const [passwordShow, setPasswrdShow] = useState(false);
   return (
-      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <Title text='Login Now'/>
-        </div>
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form className="space-y-6" onSubmit={logic.handleSubmit}>
-            <div>
-              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                Email address
-              </label>
-              <div className="mt-2">
-                <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
-                  required
-                  className="px-2 block w-full ease-in-out duration-200 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus-within:border-b-4 focus-within:border-amber-400 focus-within:outline-none  sm:text-sm sm:leading-6"
-                />
-              </div>
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <Title text="Login Now" />
+      </div>
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form className="space-y-6" onSubmit={logic.handleSubmit}>
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Email address
+            </label>
+            <div className="mt-2">
+              <input
+                id="email"
+                name="email"
+                type="email"
+                autoComplete="email"
+                required
+                className="px-2 block w-full ease-in-out duration-200 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus-within:border-b-4 focus-within:border-amber-400 focus-within:outline-none  sm:text-sm sm:leading-6"
+              />
             </div>
-            <div>
-              <div className="flex items-center justify-between">
-                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                  Password
-                </label>
-                <div className="text-sm">
-                  <a href="/forget-password" className="font-bold title">
-                    Forgot password ?
-                  </a>
-                </div>
-              </div>
-              <div className="mt-2">
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  autoComplete="current-password"
-                  required
-                  className="px-2 block w-full ease-in-out duration-200 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus-within:border-b-4 focus-within:border-amber-400 focus-within:outline-none  sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-            <div>
-              <button
-                type="submit"
-                className="w-full px-3 py-1.5 text-sm font-semibold leading-6  shadow-sm background  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 hover:scale-110 ease-in-out duration-300"
+          </div>
+          <div>
+            <div className="flex items-center justify-between">
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium leading-6 text-gray-900"
               >
-                Sign in
-              </button>
+                Password
+              </label>
+              <div className="text-sm">
+                <a href="/forget-password" className="font-bold title">
+                  Forgot password ?
+                </a>
+              </div>
             </div>
-          </form>
-        </div>
-        <Transition appear show={logic.isOpen} as={Fragment}>
-        <Dialog as="div" className="relative z-50" onClose={()=>logic.setIsOpen(false)}>
+            <div className="mt-2 relative">
+              <input
+                id="password"
+                name="password"
+                type={passwordShow ? "text" : "password"}
+                autoComplete="current-password"
+                required
+                className="px-2 block w-full ease-in-out duration-200 rounded-md border-0 py-1.5
+                   text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus-within:border-b-4
+                    focus-within:border-amber-400 focus-within:outline-none  sm:text-sm sm:leading-6"
+              />
+              <i
+                className={`absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer select-none ${
+                  passwordShow
+                    ? "fa-regular fa-eye"
+                    : "fa-regular fa-eye-slash"
+                }`}
+                onClick={() => setPasswrdShow(!passwordShow)}
+              />
+            </div>
+          </div>
+          <div>
+            <button
+              type="submit"
+              className="w-full px-3 py-1.5 text-sm font-semibold leading-6  shadow-sm background  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 hover:scale-110 ease-in-out duration-300"
+            >
+              Sign in
+            </button>
+          </div>
+        </form>
+      </div>
+      <Transition appear show={logic.isOpen} as={Fragment}>
+        <Dialog
+          as="div"
+          className="relative z-50"
+          onClose={() => logic.setIsOpen(false)}
+        >
           <Transition.Child
             as={Fragment}
             enter="ease-out duration-300"
@@ -85,17 +106,19 @@ function LogIn() {
                 leaveTo="opacity-0 scale-95"
               >
                 <Dialog.Panel className="  transform overflow-hidden rounded-2xl bg-white p-10 text-left align-middle shadow-xl transition-all">
-                <div className=' flex flex-col justify-center items-center'>
-                <i className="fa-solid fa-circle-notch animate-spin text-7xl text-yellow-500 mb-10"></i>
-                <div className='text-5xl'>Loading..<span className='animate-ping'>.</span></div>
-                </div>
+                  <div className=" flex flex-col justify-center items-center">
+                    <i className="fa-solid fa-circle-notch animate-spin text-7xl text-yellow-500 mb-10"></i>
+                    <div className="text-5xl">
+                      Loading..<span className="animate-ping">.</span>
+                    </div>
+                  </div>
                 </Dialog.Panel>
               </Transition.Child>
             </div>
           </div>
         </Dialog>
       </Transition>
-      </div>
-  )
+    </div>
+  );
 }
-export default LogIn
+export default LogIn;
--- app/components/auth/SignUp.tsx
+++ app/components/auth/SignUp.tsx
@@ -1,93 +1,125 @@
-'use client';
-import React, { Fragment } from 'react'
-import Title from '../utils/Title'
-import signUpLogic from '@/logic/auth/signUp';
-import { Dialog, Transition } from '@headlessui/react';
+"use client";
+import React, { Fragment, useState } from "react";
+import Title from "../utils/Title";
+import signUpLogic from "@/logic/auth/signUp";
+import { Dialog, Transition } from "@headlessui/react";
 function SignUp() {
-  const logic = signUpLogic()
+  const logic = signUpLogic();
+  const [passwordShow, setPasswrdShow] = useState(false);
+  const [passwordConfirmShow, setPasswordConfirmShow] = useState(false);
   return (
-      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-3 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <Title text='sign up Now'/>
-        </div>
-        <div className="mt-5 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form className="space-y-6" onSubmit={logic.handleSubmit} >
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-3 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <Title text="sign up Now" />
+      </div>
+      <div className="mt-5 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form className="space-y-6" onSubmit={logic.handleSubmit}>
           <div>
-              <label htmlFor="name" className="block text-sm font-medium leading-6 text-gray-900">
-                Name
-              </label>
-              <div className="mt-2">
-                <input
-                  id="name"
-                  name="name"
-                  type="text"
-                  autoComplete="name"
-                  required
-                  className="px-2 block w-full ease-in-out duration-200 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus-within:border-b-4 focus-within:border-amber-400 focus-within:outline-none  sm:text-sm sm:leading-6"
-                />
-              </div>
+            <label
+              htmlFor="name"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Name
+            </label>
+            <div className="mt-2">
+              <input
+                id="name"
+                name="name"
+                type="text"
+                autoComplete="name"
+                required
+                className="px-2 block w-full ease-in-out duration-200 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus-within:border-b-4 focus-within:border-amber-400 focus-within:outline-none  sm:text-sm sm:leading-6"
+              />
             </div>
-            <div>
-              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                Email address
-              </label>
-              <div className="mt-2">
-                <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
-                  required
-                  className="px-2 block w-full ease-in-out duration-200 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus-within:border-b-4 focus-within:border-amber-400 focus-within:outline-none  sm:text-sm sm:leading-6"
-                />
-              </div>
+          </div>
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Email address
+            </label>
+            <div className="mt-2">
+              <input
+                id="email"
+                name="email"
+                type="email"
+                autoComplete="email"
+                required
+                className="px-2 block w-full ease-in-out duration-200 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus-within:border-b-4 focus-within:border-amber-400 focus-within:outline-none  sm:text-sm sm:leading-6"
+              />
             </div>
-            <div>
-              <div className="flex items-center justify-between">
-                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                  Password
-                </label>
-              </div>
-              <div className="mt-2">
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  autoComplete="current-password"
-                  required
-                  className="px-2 block w-full ease-in-out duration-200 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus-within:border-b-4 focus-within:border-amber-400 focus-within:outline-none  sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-            <div>
-              <div className="flex items-center justify-between">
-                <label htmlFor="passwordConfirm" className="block text-sm font-medium leading-6 text-gray-900">
-                Password Confirm
-                </label>
-              </div>
-              <div className="mt-2">
-                <input
-                  id="passwordConfirm"
-                  name="passwordConfirm"
-                  type="password"
-                  autoComplete="current-password"
-                  required
-                  className="px-2 block w-full ease-in-out duration-200 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus-within:border-b-4 focus-within:border-amber-400 focus-within:outline-none  sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-            <div>
-              <button
-                type="submit"
-                className="w-full rounded  px-3 py-1.5 text-sm font-semibold leading-6  shadow-sm background focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 hover:scale-110 ease-in-out duration-300"
+          </div>
+          <div>
+            <div className="flex items-center justify-between">
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium leading-6 text-gray-900"
               >
-                Sign Up
-              </button>
+                Password
+              </label>
             </div>
-          </form>
-        </div>
-        <Transition appear show={logic.isOpen} as={Fragment}>
-        <Dialog as="div" className="relative z-50" onClose={()=>logic.setIsOpen(false)}>
+            <div className="mt-2 relative">
+              <input
+                id="password"
+                name="password"
+                type={passwordShow ? "text" : "password"}
+                autoComplete="current-password"
+                required
+                className="px-2 block w-full ease-in-out duration-200 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus-within:border-b-4 focus-within:border-amber-400 focus-within:outline-none  sm:text-sm sm:leading-6"
+              />
+              <i
+                className={`absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer select-none ${
+                  passwordShow ? "fa-regular fa-eye" : "fa-regular fa-eye-slash"
+                }`}
+                onClick={() => setPasswrdShow(!passwordShow)}
+              />
+            </div>
+          </div>
+          <div>
+            <div className="flex items-center justify-between">
+              <label
+                htmlFor="passwordConfirm"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Password Confirm
+              </label>
+            </div>
+            <div className="mt-2 relative">
+              <input
+                id="passwordConfirm"
+                name="passwordConfirm"
+                type={passwordConfirmShow ? "text" : "password"}
+                autoComplete="current-password"
+                required
+                className="px-2 block w-full ease-in-out duration-200 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus-within:border-b-4 focus-within:border-amber-400 focus-within:outline-none  sm:text-sm sm:leading-6"
+              />
+              <i
+                className={`absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer select-none ${
+                  passwordConfirmShow
+                    ? "fa-regular fa-eye"
+                    : "fa-regular fa-eye-slash"
+                }`}
+                onClick={() => setPasswordConfirmShow(!passwordConfirmShow)}
+              />
+            </div>
+          </div>
+          <div>
+            <button
+              type="submit"
+              className="w-full rounded  px-3 py-1.5 text-sm font-semibold leading-6  shadow-sm background focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 hover:scale-110 ease-in-out duration-300"
+            >
+              Sign Up
+            </button>
+          </div>
+        </form>
+      </div>
+      <Transition appear show={logic.isOpen} as={Fragment}>
+        <Dialog
+          as="div"
+          className="relative z-50"
+          onClose={() => logic.setIsOpen(false)}
+        >
           <Transition.Child
             as={Fragment}
             enter="ease-out duration-300"
@@ -112,17 +144,19 @@ function SignUp() {
                 leaveTo="opacity-0 scale-95"
               >
                 <Dialog.Panel className="  transform overflow-hidden rounded-2xl bg-white p-10 text-left align-middle shadow-xl transition-all">
-                <div className=' flex flex-col justify-center items-center'>
-                <i className="fa-solid fa-circle-notch animate-spin text-7xl text-yellow-500 mb-10"></i>
-                <div className='text-5xl'>Loading..<span className='animate-ping'>.</span></div>
-                </div>
+                  <div className=" flex flex-col justify-center items-center">
+                    <i className="fa-solid fa-circle-notch animate-spin text-7xl text-yellow-500 mb-10"></i>
+                    <div className="text-5xl">
+                      Loading..<span className="animate-ping">.</span>
+                    </div>
+                  </div>
                 </Dialog.Panel>
               </Transition.Child>
             </div>
           </div>
         </Dialog>
       </Transition>
-      </div>
-  )
+    </div>
+  );
 }
-export default SignUp
+export default SignUp;
