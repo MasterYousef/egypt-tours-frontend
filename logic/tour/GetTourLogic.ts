26b96daeee04114a6dc66abd52144d86c8e10c43
MasterYousef <140777033+MasterYousef@users.noreply.github.com>
2024-11-07 13:33:04 +0200

    Update GetTourLogic.ts

--- logic/tour/GetTourLogic.ts
+++ logic/tour/GetTourLogic.ts
@@ -4,7 +4,6 @@ import { tour } from '@/types/types';
 export async function GetTourLogic(id: string) {
   const res = await useGetData<tour>(`/api/v1/tour/${id}`);
   if (res.status === 'success') {
-    res.data.images?.unshift(res.data.imageCover);
     return res.data;
   } else {
     throw new Error("can't find this tour");
