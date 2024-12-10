9f28b48fbdf2d6aec25722d7b10ecf8de7ec06bd
MasterYousef <140777033+MasterYousef@users.noreply.github.com>
2024-12-10 13:39:41 +0200

    show only 3 tours

--- app/components/home/Tours.tsx
+++ app/components/home/Tours.tsx
@@ -12,7 +12,7 @@ async function Tours({user}:{user?:string}) {
       <div className={`flex flex-col justify-center items-center`}>
           <Title text='MOST POPULAR TOURS'/>
           <div className='flex flex-col md:flex-row justify-evenly items-center per mt-20 w-full'>
-            {data.map(tour => <TourCard tour={tour}/>)}
+            {data.slice(0,3).map(tour => <TourCard tour={tour}/>)}
           </div>
           {
             user === "admin" ? <Link href='Admin-Tours' className='my-16 btn text-xl p-1'>Discover all tours</Link> : <Link href='All-Tours' className='my-16 btn text-xl p-1'>Discover all tours</Link>
